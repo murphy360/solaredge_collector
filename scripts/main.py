91b66397465e416c157fd99f431ab941dab46ca6
Corey Murphy <corey.murphy@seebyte.us>
2023-12-10 15:47:55 -0800

    writing json file

--- scripts/main.py
+++ scripts/main.py
@@ -1,3 +1,4 @@
+import json
 import time
 import solar_edge_site
 import requests
@@ -7,8 +8,8 @@ import os
 # Define variables
 api_key = os.environ.get("SOLAR_EDGE_API_KEY")
 account_key = os.environ.get("SOLAREDGE_ACCOUNT_KEY")
-print("api_key) = {}".format(api_key))
-print("account_key_site) = {}".format(account_key))
+print("api_key = {}".format(api_key))
+print("account_key_site = {}".format(account_key))
 base_url = f"https://monitoringapi.solaredge.com/site/{account_key}/details?api_key={api_key}"
 print(base_url)
 site  = requests.get(base_url).json()
@@ -24,7 +25,10 @@ def main():
         today_string = datetime.date.today().isoformat()
         first_day_of_this_month = today_string[:8] + "01"
         mysite = solar_edge_site.SolarEdgeSite(site, account_key, api_key)
-        mysite.print_site()
+        mysite.print_site() 
+        # print mysite to mysite.json at /data
+        with open('/data/mysite.json', 'w') as f:
+            json.dump(mysite, f)
         mysite.get_energy_details(first_day_of_this_month, today_string)
         current_power = mysite.get_current_power()
         print("Current power: {}".format(current_power))
