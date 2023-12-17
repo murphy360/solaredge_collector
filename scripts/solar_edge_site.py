5e23776300b833e4b6b9260a7d25d38aeb3caebf
Corey Murphy <c.murphy360@gmail.com>
2023-12-16 17:15:38 -0800

    Additional Metrics

--- scripts/solar_edge_site.py
+++ scripts/solar_edge_site.py
@@ -68,11 +68,19 @@ class SolarEdgeSite:
         self.primaryModule = self.site_details['details']['primaryModule']
         self.uris = self.site_details['details']['uris']
         self.publicSettings = self.site_details['details']['publicSettings']
+
+        # Parse API Version
+        self.api_version = self.get_api_version()
         
         
         self.site_inverters = self.get_site_inverters()
         self.meters_data = self.get_meters_data()
     
+    def get_api_version(self):
+        url = "https://monitoringapi.solaredge.com/version/current"
+        api_version = requests.get(url)
+        return api_version
+
     def get_site_details(self):
         base_url = "https://monitoringapi.solaredge.com/site/{}/details?api_key={}"
         url = base_url.format(self.site_id, self.api_key)
@@ -119,6 +127,30 @@ class SolarEdgeSite:
         lifetime_energy_string = self.get_lifetime_energy_prometheus_string()
         prometheus_metrics += lifetime_energy_string + "\n"
 
+        # Write last update time to string
+        last_update_time_string = self.get_last_update_time_prometheus_string()
+        prometheus_metrics += last_update_time_string + "\n"
+
+        # Write installation date to string
+        installation_date_string = self.get_installation_date_prometheus_string()
+        prometheus_metrics += installation_date_string + "\n"
+
+        # Write site peak power to string
+        site_peak_power_string = self.get_site_peak_power_prometheus_string()
+        prometheus_metrics += site_peak_power_string + "\n"
+
+        # Write manufacturer name to string
+        manufacturer_name_string = self.get_manufacturer_name_prometheus_string()
+        prometheus_metrics += manufacturer_name_string + "\n"
+
+        # Write model name to string
+        model_name_string = self.get_model_name_prometheus_string()
+        prometheus_metrics += model_name_string + "\n"
+
+        # Write API version to string
+        API_version_string = self.get_API_version_prometheus_string()
+        prometheus_metrics += API_version_string + "\n"
+
         return prometheus_metrics
 
     def get_current_power_prometheus_string(self):
@@ -141,10 +173,80 @@ class SolarEdgeSite:
         return_string = "{}\n{}\n{}\n".format(help_string, type_string, lifetime_energy_string)
         return return_string
     
-    def get_site_peak_power_prometheus_string(self, site_id):
+    def get_site_peak_power_prometheus_string(self):
         help_string = "# HELP {}peak_power Peak Power".format(self.class_tag)
         type_string = "# TYPE {}peak_power gauge".format(self.class_tag)
         #time_epoch_now = int(datetime.datetime.now().timestamp())
         peak_power = self.peakPower
         peak_power_tag = "{{site=\"{}\"}}".format(self.site_id)
         peak_power_string = "{}peak_power{} {}".format(self.class_tag, peak_power_tag, peak_power)  
+    
+    def get_last_update_time_prometheus_string(self):
+        help_string = "# HELP {}last_update_time Last Update Time".format(self.class_tag)
+        type_string = "# TYPE {}last_update_time gauge".format(self.class_tag)
+        #time_epoch_now = int(datetime.datetime.now().timestamp())
+        last_update_time = self.lastUpdateTime
+        last_update_time_tag = "{{site=\"{}\"}}".format(self.site_id)
+        last_update_time_string = "{}last_update_time{} {}".format(self.class_tag, last_update_time_tag, last_update_time)
+        return_string = "{}\n{}\n{}\n".format(help_string, type_string, last_update_time_string)
+        return return_string
+
+    def get_installation_date_prometheus_string(self):
+        help_string = "# HELP {}installation_date Installation Date".format(self.class_tag)
+        type_string = "# TYPE {}installation_date gauge".format(self.class_tag)
+        #time_epoch_now = int(datetime.datetime.now().timestamp())
+        installation_date = self.installationDate
+        installation_date_tag = "{{site=\"{}\"}}".format(self.site_id)
+        installation_date_string = "{}installation_date{} {}".format(self.class_tag, installation_date_tag, installation_date)
+        return_string = "{}\n{}\n{}\n".format(help_string, type_string, installation_date_string)
+        return return_string
+
+    def get_installation_date_prometheus_string(self):
+        help_string = "# HELP {}installation_date Installation Date".format(self.class_tag)
+        type_string = "# TYPE {}installation_date gauge".format(self.class_tag)
+        #time_epoch_now = int(datetime.datetime.now().timestamp())
+        installation_date = self.installationDate
+        installation_date_tag = "{{site=\"{}\"}}".format(self.site_id)
+        installation_date_string = "{}installation_date{} {}".format(self.class_tag, installation_date_tag, installation_date)
+        return_string = "{}\n{}\n{}\n".format(help_string, type_string, installation_date_string)
+        return return_string
+    
+    def get_site_peak_power_prometheus_string(self):
+        help_string = "# HELP {}peak_power Peak Power".format(self.class_tag)
+        type_string = "# TYPE {}peak_power gauge".format(self.class_tag)
+        #time_epoch_now = int(datetime.datetime.now().timestamp())
+        peak_power = self.peakPower
+        peak_power_tag = "{{site=\"{}\"}}".format(self.site_id)
+        peak_power_string = "{}peak_power{} {}".format(self.class_tag, peak_power_tag, peak_power)
+        return_string = "{}\n{}\n{}\n".format(help_string, type_string, peak_power_string)
+        return return_string
+    
+    def get_manufacturer_name_prometheus_string(self):
+        help_string = "# HELP {}manufacturer_name Manufacturer Name".format(self.class_tag)
+        type_string = "# TYPE {}manufacturer_name gauge".format(self.class_tag)
+        #time_epoch_now = int(datetime.datetime.now().timestamp())
+        manufacturer_name = self.primaryModule['manufacturerName']
+        manufacturer_name_tag = "{{site=\"{}\"}}".format(self.site_id)
+        manufacturer_name_string = "{}manufacturer_name{} \"{}\"".format(self.class_tag, manufacturer_name_tag, manufacturer_name)
+        return_string = "{}\n{}\n{}\n".format(help_string, type_string, manufacturer_name_string)
+        return return_string
+    
+    def get_model_name_prometheus_string(self):
+        help_string = "# HELP {}model_name Model Name".format(self.class_tag)
+        type_string = "# TYPE {}model_name gauge".format(self.class_tag)
+        #time_epoch_now = int(datetime.datetime.now().timestamp())
+        model_name = self.primaryModule['modelName']
+        model_name_tag = "{{site=\"{}\"}}".format(self.site_id)
+        model_name_string = "{}model_name{} \"{}\"".format(self.class_tag, model_name_tag, model_name)
+        return_string = "{}\n{}\n{}\n".format(help_string, type_string, model_name_string)
+        return return_string
+    
+    def get_API_version_prometheus_string(self):
+        help_string = "# HELP {}API_version API Version".format(self.class_tag)
+        type_string = "# TYPE {}API_version gauge".format(self.class_tag)
+        #time_epoch_now = int(datetime.datetime.now().timestamp())
+        API_version = self.api_version
+        API_version_tag = "{{site=\"{}\"}}".format(self.site_id)
+        API_version_string = "{}API_version{} \"{}\"".format(self.class_tag, API_version_tag, API_version)
+        return_string = "{}\n{}\n{}\n".format(help_string, type_string, API_version_string)
+        return return_string
