bc827f34cc29628e180c4426ff266ecbcd5c37f9
Corey Murphy <c.murphy360@gmail.com>
2023-12-17 15:34:05 -0800

    reinstating the logic

--- scripts/main.py
+++ scripts/main.py
@@ -14,10 +14,6 @@ request_interval = 30 # minutes (15 minutes is the minimum allowed by the SolarE
 mysite = solar_edge_site.SolarEdgeSite(account_key, api_key)
 nominatim_service = Nominatim(user_agent='solaredge_collector')
 
-def convert_utc_to_system_time(time_string):
-    system_timezone = time.strftime("%z")
-    time_in_local_timezone = datetime.datetime.strptime(time_string, "%I:%M:%S %p") + datetime.timedelta(hours=int(system_timezone[:3]), minutes=int(system_timezone[3:]))
-    return time_in_local_timezone.strftime("%I:%M:%S %p")
 
 # Define main function
 def main():
@@ -42,10 +38,39 @@ def main():
         with open("{}site_inventory.json".format(metrics_directory), "w") as outfile:
             outfile.write(str(mysite.site_inventory))
 
-        now = datetime.datetime.now()
-        print("{}: Refreshed data. Next refresh in {} minutes.".format(now.strftime("%Y-%m-%d %H:%M:%S"), request_interval))
-        time.sleep(request_interval*60)
+        mysite.get_site_inventory()
+
+        location = nominatim_service.geocode('{}'.format(mysite.city))
+        r = requests.get('https://api.sunrise-sunset.org/json', params={'lat': location.latitude, 'lng': location.longitude, 'formatted': 0}).json()['results']
         
+        print("SunriseData: {}".format(r))
+
+        # r['sunrise'] and r['sunset'] format 2023-12-17T14:42:47+00:00
+        sunrise_datetime_utc = datetime.datetime.strptime(r['sunrise'], "%Y-%m-%dT%H:%M:%S+00:00")
+        sunset_datetime_utc = datetime.datetime.strptime(r['sunset'], "%Y-%m-%dT%H:%M:%S+00:00")
+        now_utc = datetime.datetime.now()
+        # If the sun is up, then sleep for [request_interval] minutes
+        # If the sun is down and power is less than 1, then sleep until sunrise - [request_interval] minutes
+        # SolarEdge API only updates every 15 minutes, so we shouldn't need to check more often than that
+        # SolarEdge API only allows 300 requests per day, so we shouldn't check more often than that
+
+
+        if sunrise_datetime_utc-datetime.timedelta(minutes=request_interval) < now_utc < sunset_datetime_utc:
+            print("Sun is up. Sleeping for {} minutes.".format(request_interval))
+            time.sleep(request_interval*60)
+        else:
+            print("Sun is down. Sleeping until sunrise - {} minutes.".format(request_interval))
+            # Get tomorrow's sunrise time 2023-12-17
+            tomorrow_date_string = (now_utc + datetime.timedelta(days=1)).strftime("%Y-%m-%d")
+            r = requests.get('https://api.sunrise-sunset.org/json', params={'lat': location.latitude, 'lng': location.longitude, 'formatted': 0, 'date':tomorrow_date_string}).json()['results']
+            tomorrow_sunrise_datetime = datetime.datetime.strptime(r['sunrise'], "%Y-%m-%dT%H:%M:%S+00:00")
+            print("Tomorrow's Sunrise: {}".format(tomorrow_sunrise_datetime))
+            sleep_until = tomorrow_sunrise_datetime - datetime.timedelta(minutes=request_interval)
+            print("Sleeping until: {}".format(sleep_until))
+            sleep_seconds = (sleep_until - now_utc).total_seconds()
+            print("Sleeping for {} seconds.".format(sleep_seconds))
+            time.sleep(sleep_seconds)
+            mysite.reset_api_hits()
             
 # Call main function
 main()
