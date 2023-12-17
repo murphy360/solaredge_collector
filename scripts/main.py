88dafaf5f600d63a65c554ef295860dd9415e049
Corey Murphy <c.murphy360@gmail.com>
2023-12-17 15:10:23 -0800

    Revert poll optimizer logic

--- scripts/main.py
+++ scripts/main.py
@@ -10,7 +10,7 @@ api_key = os.environ.get("SOLAR_EDGE_API_KEY")
 account_key = os.environ.get("SOLAREDGE_ACCOUNT_KEY")
 metrics_directory = "/var/www/html/"
 
-request_interval = 15 # minutes (15 minutes is the minimum allowed by the SolarEdge API)
+request_interval = 30 # minutes (15 minutes is the minimum allowed by the SolarEdge API)
 mysite = solar_edge_site.SolarEdgeSite(account_key, api_key)
 nominatim_service = Nominatim(user_agent='solaredge_collector')
 
@@ -42,42 +42,10 @@ def main():
         with open("{}site_inventory.json".format(metrics_directory), "w") as outfile:
             outfile.write(str(mysite.site_inventory))
 
-        mysite.get_site_inventory()
-
-        location = nominatim_service.geocode('{}'.format(mysite.city))
-        r = requests.get('https://api.sunrise-sunset.org/json', params={'lat': location.latitude, 'lng': location.longitude}).json()['results']
-        
-        # If the sun is up, then sleep for [request_interval] minutes
-        # If the sun is down and power is less than 1, then sleep until sunrise - [request_interval] minutes
-        # SolarEdge API only updates every 15 minutes, so we shouldn't need to check more often than that
-        # SolarEdge API only allows 300 requests per day, so we shouldn't check more often than that
-
-        # Get current time
         now = datetime.datetime.now()
+        print("{}: Refreshed data. Next refresh in {} minutes.".format(now.strftime("%Y-%m-%d %H:%M:%S"), request_interval))
+        time.sleep(request_interval*60)
         
-        # Convert sunrise and sunset to system time
-        sunrise_time = convert_utc_to_system_time(r['sunrise'])
-        sunrise_datetime = datetime.datetime.strptime(sunrise_time, "%I:%M:%S %p").replace(year=now.year, month=now.month, day=now.day)
-        print("Sunrise: {}".format(sunrise_datetime))
-        sunset_time = convert_utc_to_system_time(r['sunset'])
-        sunset_datetime = datetime.datetime.strptime(sunset_time, "%I:%M:%S %p")
-        sunset_datetime = sunset_datetime.replace(year=now.year, month=now.month, day=now.day)
-        print("Sunset: {}".format(sunset_datetime))
-
-        if sunrise_datetime < now < sunset_datetime-datetime.timedelta(minutes=request_interval):
-            print("Sun is up. Sleeping for {} minutes.".format(request_interval))
-            time.sleep(request_interval*60)
-        
-        else:
-            print("Sun is down. Sleeping until sunrise - {} minutes.".format(request_interval))
-            tomorrow_sunrise_datetime = sunrise_datetime + datetime.timedelta(days=1)
-            print("Tomorrow's Sunrise: {}".format(tomorrow_sunrise_datetime))
-            sleep_until = tomorrow_sunrise_datetime - datetime.timedelta(minutes=request_interval)
-            print("Sleeping until: {}".format(sleep_until))
-            sleep_seconds = (sleep_until - now).total_seconds()
-            print("Sleeping for {} seconds.".format(sleep_seconds))
-            time.sleep(sleep_seconds)
-            mysite.reset_api_hits()
             
 # Call main function
 main()
